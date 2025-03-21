5cff8a0f0c8efa973d06af21d08fa4fe8793b7cd
daprince1110 <198153437+daprince1110@users.noreply.github.com>
2025-03-21 03:16:17 -0500

    Dynamic Loading with NextJS. Fully finished project

---
+++ bobby-freelance/src/pages/_app.js
@@ -0,0 +1,13 @@
+import '../styles/style.css';
+import '../styles/cover.css';
+import '../styles/about.css';
+import '../styles/focus.css';
+import '../styles/gallery.css';
+import '../styles/projects.css';
+import '../styles/contact.css';
+import '../styles/responsive.css';
+
+
+export default function App({ Component, pageProps }) {
+  return <Component {...pageProps} />;
+}
